7f8872205142b00b4b9dd50a2619c39099163445
Danielle Thompson <danithompson74@gmail.com>
2020-12-11 11:53:42 -0800

    Comment out UI logic to simplest return for tests

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -15,51 +15,52 @@ public class Program
     Console.WriteLine("--------------------------");
     Console.WriteLine("What would you like to purchase today (Bread/Pastries/Exit)");
     string response = Console.ReadLine().ToLower();
+    Console.WriteLine(response);
 
-    if (response == "bread") 
-    {
-      Console.WriteLine("How many loaves of bread would you like to purchase? (e.g. 2)");
-      int breadLoaves = Console.ReadLine().ToInt();
-      // create if/ else for numbers vs anything else
-      // Console.WriteLine("Is this the correct number of bread loaves?"(Yes/No));  // stretch 
-      Console.WriteLine(breadLoaves * cost);
-      // exception handling for 0 and negative numbers - return "Not a valid #"
-      // string confirmNumLoaves = Console.ReadLine().ToLower();  // stretch
-      //   if (confirmNumLoaves == "yes")
-      //   {
-      //     return costLoaves; // TODO
-      //   }
-      //   else
-      //   {
-      //     return Program.Main(); // (stretch - go to beginning of loop)
-      //   }
-    }
-    else if (response == "pastries")
-    {
-      Console.WriteLine("How many pastries would you like to purchase? (e.g. 2)");
-      int pastryNum = Console.ReadLine().ToInt();  // (stretch - accept num value OR words translated to num) 
-      // create if/ else for numbers vs anything else
-      // Console.WriteLine("Is this the correct number of pastries?"(Yes/No)); //stretch
-      Console.WriteLine(pastryNum * cost);
-      // exception handling for 0 and negative numbers - return "Not a valid #"
-    //   string confirmNumPastries = Console.ReadLine().ToLower();  //stretch
-    //     if (confirmNumPastries == "yes")
-    //       {
-    //         return costPastries; // TODO
-    //       }
-    //     else
-    //       {
-    //         return Program.Main(); // (stretch - go to beginning of loop)
-    //       }
-    }
-    else if (response == "exit")
-    {
-      Console.WriteLine("Come again soon! Goodbye!");
-    }
-    else
-    {
-      Console.WriteLine("I'm not sure what you mean. Please try again.");
-      Program.Main();
-    }
+  //   if (response == "bread") 
+  //   {
+  //     Console.WriteLine("How many loaves of bread would you like to purchase? (e.g. 2)");
+  //     int breadLoaves = Console.ReadLine().ToInt();
+  //     // create if/ else for numbers vs anything else
+  //     // Console.WriteLine("Is this the correct number of bread loaves?"(Yes/No));  // stretch 
+  //     Console.WriteLine(breadLoaves * cost);
+  //     // exception handling for 0 and negative numbers - return "Not a valid #"
+  //     // string confirmNumLoaves = Console.ReadLine().ToLower();  // stretch
+  //     //   if (confirmNumLoaves == "yes")
+  //     //   {
+  //     //     return costLoaves; // TODO
+  //     //   }
+  //     //   else
+  //     //   {
+  //     //     return Program.Main(); // (stretch - go to beginning of loop)
+  //     //   }
+  //   }
+  //   else if (response == "pastries")
+  //   {
+  //     Console.WriteLine("How many pastries would you like to purchase? (e.g. 2)");
+  //     int pastryNum = Console.ReadLine().ToInt();  // (stretch - accept num value OR words translated to num) 
+  //     // create if/ else for numbers vs anything else
+  //     // Console.WriteLine("Is this the correct number of pastries?"(Yes/No)); //stretch
+  //     Console.WriteLine(pastryNum * cost);
+  //     // exception handling for 0 and negative numbers - return "Not a valid #"
+  //   //   string confirmNumPastries = Console.ReadLine().ToLower();  //stretch
+  //   //     if (confirmNumPastries == "yes")
+  //   //       {
+  //   //         return costPastries; // TODO
+  //   //       }
+  //   //     else
+  //   //       {
+  //   //         return Program.Main(); // (stretch - go to beginning of loop)
+  //   //       }
+  //   }
+  //   else if (response == "exit")
+  //   {
+  //     Console.WriteLine("Come again soon! Goodbye!");
+  //   }
+  //   else
+  //   {
+  //     Console.WriteLine("I'm not sure what you mean. Please try again.");
+  //     Program.Main();
+  //   }
   }
 }
