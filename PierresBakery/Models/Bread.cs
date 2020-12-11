9ab214649dccc0495d0d388fedf38be953f89d7a
Danielle Thompson <danithompson74@gmail.com>
2020-12-11 11:40:31 -0800

    Add class outlines in to bread & pastry

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -4,12 +4,19 @@ namespace Bread.Models
 {
   public class Bread
   {
-    //code goes here
+    //Buy 2, get 1 free.
+    //3rd loaf is 0 dollars - if breadLoaves % 3 == 0, totBreadCost = 10  * (breadLoaves / 3)
+    // else totBreadCost = breadLoaves * 5
+    //Single loaf = $5
   }
 
   public class Pastry
   {
-    //code goes here
+    //Buy 1 for $2
+    //1st & 2nd loaves cost $2/each
+    //Buy 3 for $5
+    //3rd loaf costs $1 - if pastryNum % 3 == 0, totCost = $5 * (pastryNum / 3)
+    //else totPastryCost = pastryNum * 2
   }
 
 }
