57abc3663beb7ae576ad55641639efc660b1536d
Danielle Thompson <danithompson74@gmail.com>
2020-12-11 10:54:57 -0800

    separate MVP from stretch goals in UI file

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -12,6 +12,7 @@ public class Program
     Console.WriteLine("Today's Daily Deals:");
     Console.WriteLine("Bread: Buy 2, get 1 free. A single loaf costs $5.");
     Console.WriteLine("Pastries: Buy 1 for $2, or 3 for $5.");
+    Console.WriteLine("--------------------------");
     Console.WriteLine("What would you like to purchase today (Bread/Pastries/Exit)");
     string response = Console.ReadLine().ToLower();
 
@@ -19,33 +20,33 @@ public class Program
     {
       Console.WriteLine("How many loaves of bread would you like to purchase?");
       int breadLoaves = Console.ReadLine();
-      Console.WriteLine("Is this the correct number of bread loaves?"(Yes/No));
-      Console.WriteLine(breadLoaves);
-      string confirmNumLoaves = Console.ReadLine().ToLower();
-        if (confirmNumLoaves == "yes")
-        {
-          return costLoaves; // TODO
-        }
-        else
-        {
-          return Program.Main(); // (stretch - go to beginning of loop)
-        }
+      // Console.WriteLine("Is this the correct number of bread loaves?"(Yes/No));  // stretch
+      Console.WriteLine(breadLoaves * cost);
+      // string confirmNumLoaves = Console.ReadLine().ToLower();  // stretch
+      //   if (confirmNumLoaves == "yes")
+      //   {
+      //     return costLoaves; // TODO
+      //   }
+      //   else
+      //   {
+      //     return Program.Main(); // (stretch - go to beginning of loop)
+      //   }
     }
     else if (response == "pastries")
     {
       Console.WriteLine("How many pastries would you like to purchase?");
       int pastries = Console.ReadLine();  // (stretch - accept num value OR words translated to num)
-      Console.WriteLine("Is this the correct number of pastries?"(Yes/No));
-      Console.WriteLine(breadLoaves);
-      string confirmNumPastries = Console.ReadLine().ToLower();
-        if (confirmNumPastries == "yes")
-          {
-            return costPastries; // TODO
-          }
-        else
-          {
-            return Program.Main(); // (stretch - go to beginning of loop)
-          }
+      // Console.WriteLine("Is this the correct number of pastries?"(Yes/No)); //stretch
+      Console.WriteLine(breadLoaves * cost);
+    //   string confirmNumPastries = Console.ReadLine().ToLower();  //stretch
+    //     if (confirmNumPastries == "yes")
+    //       {
+    //         return costPastries; // TODO
+    //       }
+    //     else
+    //       {
+    //         return Program.Main(); // (stretch - go to beginning of loop)
+    //       }
     }
     else if (response == "exit")
     {
