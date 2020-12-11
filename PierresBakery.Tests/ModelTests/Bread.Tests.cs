2722a41041f2f04ba0df072ed702ee95cdd16f65
Danielle Thompson <danithompson74@gmail.com>
2020-12-11 10:01:50 -0800

    Add test template, remove extra files

---
+++ PierresBakery.Tests/ModelTests/Bread.Tests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bread;
+
+namespace Bread.Tests
+{
+  [TestClass]
+  public class Bread_Tests
+  {
+    [TestMethod]
+    public void Test_Name()
+    {
+      //testing code will go here
+    }
+  }
+}
